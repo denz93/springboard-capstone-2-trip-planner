7c06ba72969c9252fa4af8a231d70c61194081d5
denz93 <bdnhan182@gmail.com>
2024-03-25 18:50:26 -0700

    expose /generate in dev mode to generate sample data

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,10 +2,22 @@ import { publicProcedure, router } from './trpc';
 import { db, Trip } from './db'
 import { authRouter } from './modules/auth/auth.router';
 import { tripRouter } from '@/server/modules/trip/trip.router';
+import { initData, testCleanUp } from './__tests__/helpers';
 export const appRouter = router({
   auth: authRouter,
-  trip: tripRouter
+  trip: tripRouter,
+  ...developmentRoute()
 });
+
+function developmentRoute() {
+  if (process.env.NODE_ENV === "production") return undefined
+  return {
+    generate: publicProcedure.query(async () => {
+      await testCleanUp()
+      return await initData()
+    })
+  }
+}
 
 // Export only the type of a router!
 // This prevents us from importing server code on the client.
